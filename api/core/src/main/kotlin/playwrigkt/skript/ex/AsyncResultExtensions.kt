c256d0c78e579cd25866388f82a2eb544ce682d2
Devyn Goetsch <devynmichellegoetsch@gmail.com>
2018-05-19 15:25:00 -0500

    aggregate  async result errors properly on lift()

---
+++ api/core/src/main/kotlin/playwrigkt/skript/ex/AsyncResultExtensions.kt
@@ -0,0 +1,60 @@
+package playwrigkt.skript.ex
+
+import org.funktionale.tries.Try
+import playwrigkt.skript.result.AsyncResult
+
+/**
+ * Turn a list of AsyncResults into an AsyncResult of a list
+ */
+fun <T> List<AsyncResult<out T>>.lift(): AsyncResult<List<T>> {
+    return this
+            .fold(AsyncResult.succeeded(Pair(emptyList(), emptyList())))
+            { aggregate: AsyncResult<Pair<List<T>, List<Throwable>>>, next: AsyncResult<out T> ->
+                next.also {result ->
+                    aggregate.map {(succeeded, failed) ->
+                        Pair(result.result?.let(succeeded::plus)?:succeeded, result.error?.let(failed::plus)?:failed)
+                    }
+                }
+            }
+            .flatMap {
+                if(it.second.isNotEmpty()) {
+                    AsyncResult.failed(AggregateException(it.second).flatten())
+                } else {
+                    AsyncResult.succeeded(it.first)
+                }
+            }
+}
+
+/**
+* Turn a map of AsyncResults into an AsyncResult of a map
+*/
+fun <K, V> Map<K, AsyncResult<V>>.lift(): AsyncResult<Map<K, V>> {
+    return this.entries
+            .fold(AsyncResult.succeeded(Pair(emptyMap(), emptyList())))
+            { aggregate: AsyncResult<Pair<Map<K, V>, List<Throwable>>>, next: Map.Entry<K, AsyncResult<out V>> ->
+                next.value.also { result ->
+                    aggregate.map { (succeeded, failed) ->
+                        Pair(
+                            result.result?.let { succeeded.plus(Pair(next.key, it))}?:succeeded,
+                            result.error?.let(failed::plus)?:failed
+                        )
+                    }
+                }
+            }
+            .flatMap {
+                if(it.second.isNotEmpty()) {
+                    AsyncResult.failed(AggregateException(it.second).flatten())
+                } else {
+                    AsyncResult.succeeded(it.first)
+                }
+            }
+}
+
+/**
+ * Convert a try to an asyncResult synchronously
+ */
+fun <T> Try<T>.toAsyncResult(): AsyncResult<T> =
+        when(this) {
+            is Try.Success -> AsyncResult.succeeded(this.get())
+            is Try.Failure -> AsyncResult.failed(this.throwable)
+        }
--- api/core/src/main/kotlin/playwrigkt/skript/ex/TryExtensions.kt
+++ api/core/src/main/kotlin/playwrigkt/skript/ex/TryExtensions.kt
@@ -2,40 +2,36 @@ package playwrigkt.skript.ex
 
 import org.funktionale.tries.Try
 
-data class AggregateException(val errors: List<Throwable>): Exception()
+data class AggregateException(val errors: List<Throwable>): Exception() {
+    fun flatten(): AggregateException =
+        AggregateException(this.errors.flatMap {
+            when(it) {
+                is AggregateException -> it.flatten().errors
+                else -> listOf(it)
+            }
+        })
+}
 
 fun <T: Any> List<Try<T>>.liftTry(): Try<List<T>> {
     val errors = this
             .mapNotNull { it.failed().toOption().orNull() }
-            .flatMap {
-                when {
-                    it is AggregateException -> it.errors
-                    else -> listOf(it)
-                }
-            }
 
     if(errors.isEmpty()) {
         return Try.Success(this.mapNotNull { it.toOption().orNull() })
     } else {
-        return Try.Failure(AggregateException(errors))
+        return Try.Failure(AggregateException(errors).flatten())
     }
 }
 
 fun <K: Any, V: Any> Map<K, Try<V>>.lift(): Try<Map<K, V>> {
     val errors = this
             .mapNotNull { it.value.failed().toOption().orNull() }
-            .flatMap {
-                when {
-                    it is AggregateException -> it.errors
-                    else -> listOf(it)
-                }
-            }
 
     if(errors.isEmpty()) {
         return Try.Success(this
                 .mapNotNull { entry -> entry.value.toOption().map { entry.key to it }.orNull() }
                 .toMap())
     } else {
-        return Try.Failure(AggregateException(errors))
+        return Try.Failure(AggregateException(errors).flatten())
     }
 }
