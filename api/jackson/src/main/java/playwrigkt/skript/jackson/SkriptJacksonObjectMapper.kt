6d925d00437d1fa489dfd7f8785ac44a94433eff
Devyn Goetsch <devynmichellegoetsch@gmail.com>
2018-06-02 07:22:23 -0500

    project clean up, resolve todos

--- api/jackson/src/main/java/playwrigkt/skript/jackson/SkriptJacksonObjectMapper.kt
+++ api/jackson/src/main/java/playwrigkt/skript/jackson/SkriptJacksonObjectMapper.kt
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 
+//TODo make this configurable
+//Custom deserializers
 object SkriptJacksonObjectMapper {
     fun instance(): ObjectMapper = configure(ObjectMapper())
     fun configure(objectMapper: ObjectMapper): ObjectMapper =
