bf4e045c48d0f60a97b2f5e46d3d513e9ac041b4
Devyn Goetsch <devynmichellegoetsch@gmail.com>
2018-06-17 22:40:44 -0500

    remove performer and troupes from the core api

--- api/application/src/main/kotlin/playwrigkt/skript/file/FileSkript.kt
+++ api/application/src/main/kotlin/playwrigkt/skript/file/FileSkript.kt
@@ -1,13 +1,10 @@
 package playwrigkt.skript.file
 
-import arrow.core.Try
 import playwrigkt.skript.Skript
 import playwrigkt.skript.result.AsyncResult
 import playwrigkt.skript.troupe.FileTroupe
 import java.io.BufferedReader
 import java.io.BufferedWriter
-import java.nio.file.Path
-import java.nio.file.Paths
 
 sealed class FileSkript<I, O>: Skript<I, O, FileTroupe> {
     object Reader: FileSkript<FileReference, BufferedReader>() {
@@ -27,17 +24,3 @@ sealed class FileSkript<I, O>: Skript<I, O, FileTroupe> {
     }
 }
 
-sealed class FileReference {
-    abstract fun toPath(): Try<Path>
-    data class Relative(val path: String): FileReference() {
-        override fun toPath(): Try<Path> {
-            return Try { Paths.get(path) }
-        }
-    }
-    data class Absolute(val path: String): FileReference() {
-        override fun toPath(): Try<Path> {
-            return Try { Paths.get(path) }
-        }
-
-    }
-}
